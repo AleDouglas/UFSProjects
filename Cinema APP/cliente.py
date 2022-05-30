eef09af01c41dabc89414a7e62fba338aadc8ef4
lomps-droid <xand.douglass2@gmail.com>
2022-05-29 23:08:09 -0300

    Add New Project
    Projeto de Cinema adicionado ao GITHUB

---
+++ Cinema APP/cliente.py
@@ -0,0 +1,6 @@
+class Cliente:
+    def __init__(self, id_film, date, hour):
+        self.__id = id_film
+        self.__date = date
+        self.__hour = hour
+        self.__clientes = [] # []
---
+++ Cinema APP/main.py
@@ -0,0 +1,8 @@
+from ui_functions import *
+
+
+if __name__ == "__main__":
+    qt = QApplication(sys.argv)
+    app_open = App()
+    app_open.show()
+    qt.exec_()
